8dd550a63aa999238e4f24040ba555ec85e26661
leonardo <leonardocolodette0@gmail.com>
2023-08-14 15:32:54 -0300

    feat: create theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  fontSize: {
+    xs: 13,
+    sm: 17,
+    md: 19,
+    lg: 24,
+    xl: 60,
+  },
+  fontFamily: {
+    bold: 'TT Commons DemiBold',
+    regular: 'TT Commons Regular',
+  },
+  color: {
+    primary: '#52a8c9',
+    light: '#FFFFFF',
+    black: '#000000',
+    'sea-green': '#075669',
+  },
+};
