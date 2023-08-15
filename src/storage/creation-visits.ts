c9ce19561cd586616048d40b80c7067cae43a233
leonardo <leonardocolodette0@gmail.com>
2023-08-15 11:39:02 -0300

    feat: add comments to module CreationVisitsStorage.

--- src/storage/creation-visits.ts
+++ src/storage/creation-visits.ts
@@ -1,3 +1,11 @@
+/**
+ * This module is responsible for persisting the count of views on the creation page.
+ *
+ * Basically, we have an object where each key corresponds to the creation's id and the value
+ * represents the quantity of views on the page. The MMKV storage is used to persist this object.
+ *
+ */
+
 import { MMKV } from 'react-native-mmkv';
 
 export class CreationVisitsStorage {
@@ -8,6 +16,12 @@ export class CreationVisitsStorage {
     this.storage = new MMKV();
   }
 
+  /**
+   * Get information about the visits on the creation pages.
+   *
+   * @memberOf CreationVisitsStorage
+   * @returns {Object} Returns the creation visits
+   */
   get creationVisits() {
     try {
       const jsonCreationVisits = this.storage.getString(this.storageKey);
@@ -17,6 +31,13 @@ export class CreationVisitsStorage {
     }
   }
 
+  /**
+   * Increase the count of visits to a creation page
+   *
+   * @memberOf CreationVisitsStorage
+   * @param {number} id The creation's id
+   * @returns {number} Returns the current quantity of vistis.
+   */
   increaseVisitCount(id: number): number {
     const value = this.creationVisits[id] ? this.creationVisits[id] + 1 : 1;
     const data = JSON.stringify({ ...this.creationVisits, [id]: value });
